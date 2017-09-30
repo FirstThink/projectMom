662dc82f4dc97ef6b4158df18e6a63167aeb100f
FirstThink <sbhuh@live.com>
2017-09-29 21:14:52 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-from bottle import run, route, get, post, request # or route
+from bottle import run, get, post, request
 
 @get('/')
 @get('/home')
@@ -10,6 +10,13 @@ def default_page():
 			<input value="Submit" type="submit">
 		</form>
 		'''
-
+@post('/home')							# submit button induces the call of search_query()
+def search_query():
+	query = request.forms.get('query')	# input string stored in the variable QUERY
+	if query == '':						# if there is no input
+		return "<p>Please enter something.</p>"
+	else:
+		# where all the results go (tables, top 20 history, URLs, etc)
+		return query
+		
 run(host='localhost', port=8080, debug=True)	# runs the server
-
